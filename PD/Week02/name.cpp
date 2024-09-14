3dede9863482087815badde3b4fc6ac354e3d417
ZainUET <zain41uet@gmail.com>
2024-09-14 13:01:58 +0500

    Add files via upload

---
+++ PD/Week02/name.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+main()
+{  system("color 3");
+   system("cls");
+  cout<<"    #################                **               ###############       #              #                                                                                     "<<endl;
+  cout<<"                  #                  ##                      #              # #            #                                  "<<endl;
+  cout<<"                #                   #  #                     #              #   #          #                                  "<<endl;
+  cout<<"              #                    #    #                    #              #     #        #                                  "<<endl;
+  cout<<"            #                     #      #                   #              #       #      #                                  "<<endl;
+  cout<<"          #                      ##########                  #              #         #    #                                  "<<endl;
+  cout<<"        #                       #          #                 #              #           #  #                                  "<<endl;
+  cout<<"      #                        #            #                #              #             ##                                  "<<endl;
+  cout<<"    #################         #              #        ###############       #              #                                  "<<endl;
+  
+
+
+
+
+
+
+}
