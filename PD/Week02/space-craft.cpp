0d1569fe8877e3cae05873c91d9b2e917785b635
ZainUET <zain41uet@gmail.com>
2024-09-14 13:45:00 +0500

    Add files via upload

---
+++ PD/Week02/space-craft.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+main(){ 
+     system("cls");
+     system("color 64");
+  cout<<"                 /\\                                        "<<endl;
+  cout<<"              __/~~\\__                                     "<<endl;
+  cout<<"            /   |  |   \\                                   "<<endl;
+  cout<<"           =====.  .=====                                  "<<endl;
+  cout<<"                ||||                                       "<<endl;
+  
+
+}
