5db05dce50995c4c964d6a6f4a1ebc8e35e03cee
ZainUET <zain41uet@gmail.com>
2024-09-16 22:08:05 +0500

    Add files via upload

---
+++ PD/Week02/pistol.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+main(){
+       system("cls");
+       system("color 03");
+  
+   cout<<"   +__^_________________________,___________________,___________,___________^_                                                                                                                 "<<endl;
+   cout<<"    '                                                                          '                                    "<<endl;
+   cout<<"    |   |||||||||||||||||||||    '____________________'            |           O                                    "<<endl;
+   cout<<"    '                                                                          '                                    "<<endl;
+   cout<<"    '+_____________________________________________________________^___________|                                    "<<endl;
+   cout<<"     '                                                                                                              "<<endl;
+   cout<<"      ',----------------------,-----------------------,--------------------'                                        "<<endl;
+   cout<<"         /  XXXXXXXXXXXX      /''\\          //                                                                       "<<endl;
+   cout<<"        /                    /    \\        //                                                                        "<<endl;
+   cout<<"       /  XXXXXXXXXXXXX     /      \\      //                                                                         "<<endl;
+   cout<<"      /                    /'------------'                                                                          "<<endl;
+   cout<<"     /  XXXXXXXXXXXXX     /                                                                                         "<<endl;
+   cout<<"    /                    /                                                                                          "<<endl;
+   cout<<"   /  XXXXXXXXXXXXX     /                                                                                           "<<endl;
+   cout<<"  /   ------------     /                                                                                            "<<endl;
+   cout<<" /  XXXXXXXXXXXXXX    /                                                                                             "<<endl;
+   cout<<" (___________________(                                                                                              "<<endl;
+   cout<<"                                                                                                                    "<<endl;
+   cout<<"             A CUSTOMIZED PISTOL                                                                                    "<<endl;
+   
+
+}
